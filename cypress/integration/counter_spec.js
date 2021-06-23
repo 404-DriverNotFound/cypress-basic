1dbc6b9ddd30f54a416a19079c0ab7f8830f29cd
hyo-choi <hyochoi31@gmail.com>
2021-06-24 02:19:09 +0900

    refactor: 테스트 로직 수정 및 describe it 문구 변경

--- cypress/integration/counter_spec.js
+++ cypress/integration/counter_spec.js
@@ -1,39 +1,39 @@
-describe('ui counter test', () => {
+describe('UI counter', () => {
   const plusButtonClass = '.plus-button';
   const minusButtonClass = '.minus-button';
   const countDisplay = '.count-display';
   const checkCounter = (buttonClass, expectedValue) => {
-      cy.get(buttonClass).click();
-      cy.get(buttonClass).get(countDisplay).should('have.value', expectedValue);
+    cy.get(buttonClass).click()
+      .get(countDisplay).should('have.value', expectedValue);
   };
 
-  it('check button rendered', function () {
+  it('renders button', function () {
     cy.visit('/').get('.counter');
   });
 
-  it('check initial value 10', function () {
+  it('initialize input value with 10', function () {
     cy.get(countDisplay).should('have.value', '10');
   });
 
-  it('check plus button behavior', function () {
+  it('increases value by 1 when plus-button clicked', function () {
     checkCounter(plusButtonClass, '11');
     checkCounter(plusButtonClass, '12');
   });
 
-  it('check minus button behavior', function () {
+  it('decreases value by 1 when minus-button clicked', function () {
     cy.reload();
     checkCounter(minusButtonClass, '9');
     checkCounter(minusButtonClass, '8');
   });
 
-  it('check plus button max value 12', function () {
+  it('doesn\'t increase value over 12', function () {
     cy.reload();
     checkCounter(plusButtonClass, '11');
     checkCounter(plusButtonClass, '12');
     checkCounter(plusButtonClass, '12');
   });
 
-  it('check minus button min value 8', function () {
+  it('doesn\'t decrease value under 8', function () {
     cy.reload();
     checkCounter(minusButtonClass, '9');
     checkCounter(minusButtonClass, '8');
